765dc585b0765d80c0597a62ea945446d4935272
aungminkhant <amk.never.gives.up@gmail.com>
2026-09-02 00:07:54 +0630

    Configure astro config for Github pages, add .gitignore

---
+++ README.md
@@ -0,0 +1,7 @@
+# NOTES TO MYSELF
+
+When base is configured, all of your internal page links must be prefixed with your base value:
+
+```html
+<a href="/fruitiganza/about">About</a>
+```
--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://aung-mkhant.github.io",
+  base: "/fruitiganza",
   build: {
     inlineStylesheets: "never",
   },
