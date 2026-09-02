380da2cbc3512ff3ab357eab47bf084d7a20a0b7
aungminkhant <amk.never.gives.up@gmail.com>
2026-09-02 13:10:52 +0630

    Add dynamic links showcase, rename project

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from "astro/config"
 // https://astro.build/config
 export default defineConfig({
   site: "https://aung-mkhant.github.io",
-  base: "/fruitiganza",
+  base: "/design-lab",
   build: {
-    inlineStylesheets: "never",
-  },
+    inlineStylesheets: "never"
+  }
 })
